dfed5220e6eaf2edd0517725eb023b1519d3429b
imakshat <47633507+imakshat47@users.noreply.github.com>
2019-09-29 11:35:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,55 @@
 Akshat Here!!
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+
+        <!--jQuery library--> 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+        <!--Latest compiled and minified JavaScript--> 
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--External style sheet-->
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <title>Lifestyle Store</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button> 
+                    <a class="navbar-brand" href="#">Lifestyle Store</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="signup.html"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                        <li><a href="login.html"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        <li><a href="http://myprojects.gq/"><span class="glyphicon glyphicon-folder-open">  My Projects</span>  </a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <content>
+            <div id="banner_image">
+                <div class="container">
+                    <div id="banner_content">
+                        <a class="btn btn-danger btn-lg active" href="product.html">Shop Now</a>
+                    </div>
+                </div>
+            </div>
+        </content>
+        <footer>
+            <div class="container">
+                <center>
+                    Copyright © Lifestyle Store. All Rights Reserved | Contact Us: +91 90000 00000
+                </center>
+            </div>
+        </footer>
+    </body>
+</html>
